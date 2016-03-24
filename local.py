422d34e69c2c0b620297a607efa3972a4a0ede31
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-24 01:03:08 +0100

    add smtp

--- local.py
+++ local.py
@@ -26,14 +26,12 @@ DATABASES = {
 	}
 }
 
-# Uncomment and populate with proper connection parameters
-# for enable email sending. EMAIL_HOST_USER should end by @domain.tld
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_USE_TLS = False
-# EMAIL_HOST = "localhost"
-# EMAIL_HOST_USER = ""
-# EMAIL_HOST_PASSWORD = ""
-# EMAIL_PORT = 25
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = False
+EMAIL_HOST = "smtp.default.svc.cluster.local"
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 # Uncomment and populate with proper connection parameters
 # for enable github login/singin.
