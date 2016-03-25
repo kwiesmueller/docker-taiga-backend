ad448b0a4388e87ba15df77d06ec0c5b0fd21131
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-25 15:25:47 +0100

    add default_from

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ smtp_port=${SMTP_PORT:-'25'}
 secret_key=${SECRET_KEY:-'Z2nwo3KDU24Qie7jX2uStFuukaJ92JFO'}
 
 sed_script=""
-for var in database_host database_name database_user database_password database_port smtp_host smtp_port secret_key; do
+for var in default_from database_host database_name database_user database_password database_port smtp_host smtp_port secret_key; do
   sed_script+="s,{{$var}},${!var},g;"
 done
 
